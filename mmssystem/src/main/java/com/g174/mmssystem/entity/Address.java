e4a8e6e8c4ca1611b6ef20ef45bcb56af4bc93a8
hunghnhe187092 <hunghnhe187092@fpt.edu.vn>
2025-10-11 04:51:15 +0700

    Create Address.java

---
+++ mmssystem/src/main/java/com/g174/mmssystem/entity/Address.java
@@ -0,0 +1,48 @@
+package com.g174.mmssystem.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "addresses")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
+    private Integer addressId;
+
+    @Size(max = 255)
+    @Column(name = "street")
+    private String street;
+
+    @Size(max = 100)
+    @Column(name = "city")
+    private String city;
+
+    @Size(max = 100)
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = Instant.now();
+        updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = Instant.now();
+    }
+}
