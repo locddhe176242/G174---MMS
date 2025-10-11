d48ffedf1afb503f171d79e43a97023e4ff01e10
locddhe176242 <locddhe176242@fpt.edu.vn>
2025-10-11 19:33:34 +0700

    update api verifiOTP and authStore

--- mmssystem/src/main/java/com/g174/mmssystem/config/SecurityConfig.java
+++ mmssystem/src/main/java/com/g174/mmssystem/config/SecurityConfig.java
@@ -59,9 +59,8 @@ public class SecurityConfig {
                                 "/api/auth/login",
                                 "/api/auth/refresh",
                                 "/api/auth/forgot-password/request",
+                                "/api/auth/forgot-password/verify-otp",
                                 "/api/auth/forgot-password/verify",
-                                "/error",
-                                "/actuator/health",
                                 "/api/customers/**",
                                 "/api/vendors/**"
                         ).permitAll()
