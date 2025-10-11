52d55d539f2cbcbee30dbb114797058fdf532714
locddhe176242 <locddhe176242@fpt.edu.vn>
2025-10-11 19:37:57 +0700

    add DTOverifiOtp Response

---
+++ mmssystem/src/main/java/com/g174/mmssystem/dto/auth/VerifyOtpOnlyResponseDTO.java
@@ -0,0 +1,15 @@
+package com.g174.mmssystem.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerifyOtpOnlyResponseDTO {
+    private String message;
+    private int remainingAttempts;
+}
