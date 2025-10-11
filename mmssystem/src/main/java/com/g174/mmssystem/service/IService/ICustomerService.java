bc8abe44dcd131511791ce70e576ea6e542916ed
hunghnhe187092 <hunghnhe187092@fpt.edu.vn>
2025-10-11 22:23:34 +0700

    update getCustomerDetailById DTO

--- mmssystem/src/main/java/com/g174/mmssystem/service/IService/ICustomerService.java
+++ mmssystem/src/main/java/com/g174/mmssystem/service/IService/ICustomerService.java
@@ -1,6 +1,8 @@
 package com.g174.mmssystem.service.IService;
 
+import com.g174.mmssystem.dto.requestDTO.CustomerFormRequestDTO;
 import com.g174.mmssystem.dto.requestDTO.CustomerRequestDTO;
+import com.g174.mmssystem.dto.responseDTO.CustomerDetailResponseDTO;
 import com.g174.mmssystem.dto.responseDTO.CustomerResponseDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,4 +19,6 @@ public interface ICustomerService {
     CustomerResponseDTO updateCustomer(Integer customerId, CustomerRequestDTO customerRequestDTO);
     void deleteCustomer(Integer customerId);
     void restoreCustomer(Integer customerId);
+    CustomerDetailResponseDTO getCustomerDetailById(Integer customerId);
+    CustomerResponseDTO updateCustomerForm(Integer customerId, CustomerFormRequestDTO customerFormRequestDTO);
 }
