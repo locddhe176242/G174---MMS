2b17cc624d248528fbde83be1de39bbc8eda4366
hunghnhe187092 <hunghnhe187092@fpt.edu.vn>
2025-10-21 12:49:12 +0700

    Update ContactDTO.java
    delete validate

--- mmssystem/src/main/java/com/g174/mmssystem/dto/requestDTO/ContactDTO.java
+++ mmssystem/src/main/java/com/g174/mmssystem/dto/requestDTO/ContactDTO.java
@@ -7,13 +7,10 @@ import lombok.Data;
 
 @Data
 public class ContactDTO {
-    @Size(max = 50)
     private String phone;
 
     @Size(max = 100)
-    @Email(message = "Email không đúng định dạng (vd: example@gmail.com)")
-    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
-            message = "Email không hợp lệ")
+    
     private String email;
 
     @Size(max = 255)
