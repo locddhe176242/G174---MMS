8c0b9e50952731e21e82f88cb031d3e2b1121847
locddhe176242 <locddhe176242@fpt.edu.vn>
2025-10-26 13:10:21 +0700

    feat: Add Spring Boot main application class
    - Create MmssystemApplication.java as application entry point
    - Configure @SpringBootApplication annotation
    - Add main method for application startup

--- mmssystem/src/main/java/com/g174/mmssystem/MmssystemApplication.java
+++ mmssystem/src/main/java/com/g174/mmssystem/MmssystemApplication.java
@@ -2,14 +2,12 @@ package com.g174.mmssystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class MmssystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MmssystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MmssystemApplication.class, args);
+    }
 
 }
---
+++ mmssystem/src/main/java/com/g174/mmssystem/repository/ActivityLogRepository.java
@@ -0,0 +1,2 @@
+package com.g174.mmssystem.repository;public interface ActivityLogRepository {
+}
