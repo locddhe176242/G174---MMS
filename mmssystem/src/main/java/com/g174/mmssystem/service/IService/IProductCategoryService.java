318d69e19dd02fa81bc70f847a4e88f82b8961df
locddhe176242 <locddhe176242@fpt.edu.vn>
2025-10-31 12:17:13 +0700

    feat(be): define ProductCategory service interface

--- mmssystem/src/main/java/com/g174/mmssystem/service/IService/IProductCategoryService.java
+++ mmssystem/src/main/java/com/g174/mmssystem/service/IService/IProductCategoryService.java
@@ -1,6 +1,24 @@
 package com.g174.mmssystem.service.IService;
 
-public interface IProductCategoryService {
-    Object getProductCategories();
+import com.g174.mmssystem.dto.requestDTO.ProductCategoryRequestDTO;
+import com.g174.mmssystem.dto.responseDTO.ProductCategoryResponseDTO;
+import jakarta.validation.Valid;
 
+import java.util.List;
+
+public interface IProductCategoryService {
+
+    List<ProductCategoryResponseDTO> getProductCategories();
+
+    List<ProductCategoryResponseDTO> getDeletedProductCategories();
+
+    ProductCategoryResponseDTO getProductCategory(Integer id);
+
+    ProductCategoryResponseDTO createProductCategory(@Valid ProductCategoryRequestDTO request);
+ 
+    ProductCategoryResponseDTO updateProductCategory(Integer id, @Valid ProductCategoryRequestDTO request);
+
+    void deleteProductCategory(Integer id);
+
+    void restoreProductCategory(Integer id);
 }
