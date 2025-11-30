c1e60179f8183ccb2e06039df0dd3cd359de42f8
locddhe176242 <locddhe176242@fpt.edu.vn>
2025-11-30 21:40:01 +0700

    feat(frontend): Add routes for Invoice, Return Order and Credit Note modules

--- MMS-FE/src/App.jsx
+++ MMS-FE/src/App.jsx
@@ -33,6 +33,18 @@ import SalesQuotationDetail from "./compnents/pages/Sale/SalesQuotation/SalesQuo
 import SalesOrderList from "./compnents/pages/Sale/SalesOrder/SalesOrderList.jsx";
 import SalesOrderForm from "./compnents/pages/Sale/SalesOrder/SalesOrderForm.jsx";
 import SalesOrderDetail from "./compnents/pages/Sale/SalesOrder/SalesOrderDetail.jsx";
+import DeliveryList from "./compnents/pages/Sale/Delivery/DeliveryList.jsx";
+import DeliveryForm from "./compnents/pages/Sale/Delivery/DeliveryForm.jsx";
+import DeliveryDetail from "./compnents/pages/Sale/Delivery/DeliveryDetail.jsx";
+import ReturnOrderList from "./compnents/pages/Sale/ReturnOrder/ReturnOrderList.jsx";
+import ReturnOrderForm from "./compnents/pages/Sale/ReturnOrder/ReturnOrderForm.jsx";
+import ReturnOrderDetail from "./compnents/pages/Sale/ReturnOrder/ReturnOrderDetail.jsx";
+import CreditNoteList from "./compnents/pages/Sale/CreditNote/CreditNoteList.jsx";
+import CreditNoteForm from "./compnents/pages/Sale/CreditNote/CreditNoteForm.jsx";
+import CreditNoteDetail from "./compnents/pages/Sale/CreditNote/CreditNoteDetail.jsx";
+import InvoiceList from "./compnents/pages/Sale/Invoice/InvoiceList.jsx";
+import InvoiceForm from "./compnents/pages/Sale/Invoice/InvoiceForm.jsx";
+import InvoiceDetail from "./compnents/pages/Sale/Invoice/InvoiceDetail.jsx";
 import ApprovalList from "./compnents/pages/Admin/ApprovalList.jsx";
 
 import RoleManagement from "./compnents/pages/Admin/RoleManagement";
@@ -90,6 +102,22 @@ function App() {
                 <Route path="/sales/orders/new" element={<SalesOrderForm />} />
                 <Route path="/sales/orders/:id" element={<SalesOrderDetail />} />
                 <Route path="/sales/orders/:id/edit" element={<SalesOrderForm />} />
+                <Route path="/sales/deliveries" element={<DeliveryList />} />
+                <Route path="/sales/deliveries/new" element={<DeliveryForm />} />
+                <Route path="/sales/deliveries/:id" element={<DeliveryDetail />} />
+                <Route path="/sales/deliveries/:id/edit" element={<DeliveryForm />} />
+                <Route path="/sales/return-orders" element={<ReturnOrderList />} />
+                <Route path="/sales/return-orders/new" element={<ReturnOrderForm />} />
+                <Route path="/sales/return-orders/:id" element={<ReturnOrderDetail />} />
+                <Route path="/sales/return-orders/:id/edit" element={<ReturnOrderForm />} />
+                <Route path="/sales/credit-notes" element={<CreditNoteList />} />
+                <Route path="/sales/credit-notes/new" element={<CreditNoteForm />} />
+                <Route path="/sales/credit-notes/:id" element={<CreditNoteDetail />} />
+                <Route path="/sales/credit-notes/:id/edit" element={<CreditNoteForm />} />
+                <Route path="/sales/invoices" element={<InvoiceList />} />
+                <Route path="/sales/invoices/new" element={<InvoiceForm />} />
+                <Route path="/sales/invoices/:id" element={<InvoiceDetail />} />
+                <Route path="/sales/invoices/:id/edit" element={<InvoiceForm />} />
                 <Route path="/approvals" element={<ApprovalList />} />
 
                 <Route path="/admin/roles" element={<RoleManagement />} />
