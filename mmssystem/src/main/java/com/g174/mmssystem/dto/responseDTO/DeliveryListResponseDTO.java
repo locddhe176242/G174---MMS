9995b21c0a600a40ea91734b09f820daf92cbbdd
locddhe176242 <locddhe176242@fpt.edu.vn>
2025-11-30 21:43:44 +0700

    feat(backend): Update Delivery response DTOs

--- mmssystem/src/main/java/com/g174/mmssystem/dto/responseDTO/DeliveryListResponseDTO.java
+++ mmssystem/src/main/java/com/g174/mmssystem/dto/responseDTO/DeliveryListResponseDTO.java
@@ -1,2 +1,31 @@
-package com.g174.mmssystem.dto.responseDTO;public class DeliveryListResponseDTO {
+package com.g174.mmssystem.dto.responseDTO;
+
+import com.g174.mmssystem.entity.Delivery;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+@Builder
+public class DeliveryListResponseDTO {
+
+    private Integer deliveryId;
+    private String deliveryNo;
+    private Delivery.DeliveryStatus status;
+    private String trackingCode;
+    private Integer salesOrderId;
+    private String salesOrderNo;
+    private Integer customerId;
+    private String customerName;
+    private Integer warehouseId;
+    private String warehouseName;
+    private Instant plannedDate;
+    private Instant actualDate;
+
+    private Instant createdAt;
+    private Instant updatedAt;
+    private String createdByDisplay;
+    private String updatedByDisplay;
 }
+
--- mmssystem/src/main/java/com/g174/mmssystem/dto/responseDTO/DeliveryResponseDTO.java
+++ mmssystem/src/main/java/com/g174/mmssystem/dto/responseDTO/DeliveryResponseDTO.java
@@ -1,2 +1,43 @@
-package com.g174.mmssystem.dto.responseDTO;public class DeliveryResponseDTO {
+package com.g174.mmssystem.dto.responseDTO;
+
+import com.g174.mmssystem.entity.Delivery;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.List;
+
+@Data
+@Builder
+public class DeliveryResponseDTO {
+
+    private Integer deliveryId;
+    private String deliveryNo;
+    private Delivery.DeliveryStatus status;
+
+    private Integer salesOrderId;
+    private String salesOrderNo;
+    private Integer customerId;
+    private String customerName;
+
+    private Integer warehouseId;
+    private String warehouseName;
+
+    private Instant plannedDate;
+    private Instant actualDate;
+
+    private String shippingAddress;
+    private String carrierName;
+    private String driverName;
+    private String driverPhone;
+    private String trackingCode;
+    private String notes;
+
+    private Instant createdAt;
+    private String createdBy;
+    private Instant updatedAt;
+    private String updatedBy;
+
+    private List<DeliveryItemResponseDTO> items;
 }
+
