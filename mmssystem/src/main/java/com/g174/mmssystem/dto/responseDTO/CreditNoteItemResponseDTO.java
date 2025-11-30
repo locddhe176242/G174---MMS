4216bd8f32bc778e67c77777eeb469c8908a29fc
locddhe176242 <locddhe176242@fpt.edu.vn>
2025-11-30 21:38:20 +0700

    feat(backend): Add CreditNote response DTOs

---
+++ mmssystem/src/main/java/com/g174/mmssystem/dto/responseDTO/CreditNoteItemResponseDTO.java
@@ -0,0 +1,29 @@
+package com.g174.mmssystem.dto.responseDTO;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+public class CreditNoteItemResponseDTO {
+
+    private Integer cniId;
+
+    private Integer productId;
+    private String productSku;
+    private String productCode;
+    private String productName;
+    private String uom;
+
+    private BigDecimal quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal discountAmount;
+    private BigDecimal taxRate;
+    private BigDecimal taxAmount;
+    private BigDecimal lineTotal;
+
+    private String note;
+}
+
